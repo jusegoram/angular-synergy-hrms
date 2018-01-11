46aa57675acc58e4ccae392788fb9a7566e04d8b
Sebastian <jseb16@gmail.com>
2018-01-11 12:38:21 -0500

    fixed detail component issues, enabled dropdown menus for family and payroll info

--- src/app/employee/Employee.ts
+++ src/app/employee/Employee.ts
@@ -5,9 +5,12 @@ export class IEmployee {
               public lastName: string,
               public birthDate: Date,
               public socialSecurity: string,
+              public client: string,
               public campaign: string,
               public positionid: number,
               public positionName: string,
               public status: string,
+              public hireDate: Date,
+              public terminationDate?: Date,
             ) { }
   }
