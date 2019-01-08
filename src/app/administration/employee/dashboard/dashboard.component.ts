a8f7f5b6f6ccbec623f631c2894ef5c602c9dc9b
Juan Sebastian Gomez <jseb16@gmail.com>
2019-01-07 22:13:11 -0600

    small bugfix

--- src/app/administration/employee/dashboard/dashboard.component.ts
+++ src/app/administration/employee/dashboard/dashboard.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  dataSource: any;
+  displayedColumns = [];
+  data: any;
   constructor() { }
 
   ngOnInit() {
