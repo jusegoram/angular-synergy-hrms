c8002863c381cedee4e9e98a7b84c30d0ba39d7a
hcorazao <6912543+hcorazao@users.noreply.github.com>
2020-03-20 13:51:43 -0500

    certify tracker dialog components added
    -certification date and validator added
    -manage signature added

--- src/app/employee/detail/certify-dialog/certify-dialog.component.ts
+++ src/app/employee/detail/certify-dialog/certify-dialog.component.ts
@@ -1,17 +1,28 @@
 import {Component, Inject} from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-certify-dialog',
   templateUrl: './certify-dialog.component.html',
   styleUrls: ['./certify-dialog.component.scss']
 })
 export class CertifyDialogComponent {
-
+  certifyForm: FormGroup;
   constructor( public dialogRef: MatDialogRef<CertifyDialogComponent>,
+    private formBuilder: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: any) {}
 
   ngOnInit() {
+    this.certifyForm = this.formBuilder.group({
+      certificationDate: [new Date(), Validators.required],
+      managerSignature: ['', Validators.required],
+    });
   }
+
+  get certificationDateHasError(){
+    return this.certifyForm.get('certificationDate').invalid;
+  }
+
   onProceedClick(): void {
     this.dialogRef.close(true);
   }
