25048a9b4e75f1710afaa39d728e48b6f15beaf9
hcorazao <6912543+hcorazao@users.noreply.github.com>
2020-04-04 21:23:14 -0500

    tracker types details modified

--- src/app/hr/components/tracker-status-change-details/tracker-status-change-details.component.ts
+++ src/app/hr/components/tracker-status-change-details/tracker-status-change-details.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Tracker } from '../../../shared/models/tracker';
+import { Chance } from '../../../shared/models/chance';
 
 @Component({
   selector: 'app-tracker-status-change-details',
   templateUrl: './tracker-status-change-details.component.html',
   styleUrls: ['./tracker-status-change-details.component.scss']
 })
-export class TrackerStatusChangeDetailsComponent implements OnInit {
-  @Input() trackerInfo: Tracker;
+export class TrackerStatusChangeDetailsComponent implements OnInit, OnChanges {
+  @Input() trackerInfo: Tracker;  
   constructor() { }
-
-  ngOnInit(): void {
+  
+  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
+    throw new Error("Method not implemented.");
   }
 
+  ngOnInit(): void {
+  }  
+
+  get chances():Array<{nth:string, details:Chance}>{
+    return [
+      {
+        nth: 'First',
+        details:this.trackerInfo?.statusChange?.absenteeism.firstChance,
+      },
+      {
+        nth: 'Second',
+        details:this.trackerInfo?.statusChange?.absenteeism.secondChance,
+      },
+      {
+        nth: 'Third',
+        details:this.trackerInfo?.statusChange?.absenteeism.secondChance,
+      }
+    ];
+  }
 }
